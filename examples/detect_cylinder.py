bc089d60d4de65a08facb9fa6a4685a6f90bc8bc
evbernardes <evbernardes@gmail.com>
2023-10-10 11:27:42 +0200

    added simple cylinder example

---
+++ examples/detect_cylinder.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 15:30:28 2023
+
+@author: ebernardes
+"""
+
+import copy
+import time
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import open3d as o3d
+from open3d.visualization import draw_geometries
+
+# from helpers import color_blue, color_gray, color_red, color_yellow
+from pyShapeDetector.primitives import Sphere, Plane, Cylinder
+
+from pyShapeDetector.methods import RANSAC_Classic, RANSAC_Weighted, MSAC, BDSAC, LDSAC
+methods = [RANSAC_Classic, 
+           RANSAC_Weighted,
+           MSAC,
+           BDSAC,
+           LDSAC]
+
+#%% Parameters and input
+method = methods[0]
+filedir = Path('./data')
+filename = '1cylinders'
+pcd = o3d.io.read_point_cloud(str((filedir / filename).with_suffix('.pcd')))
+normals = pcd.estimate_normals()
+
+#%%
+inliers_min = 200
+
+detector = method(Cylinder, num_iterations=15,
+                  threshold_angle=30,
+                  # max_point_distance=0.5,
+                  inliers_min=inliers_min)
+
+shape, inliers, metrics = detector.fit(pcd.points, debug=False, normals=normals)
+pcd_shape = pcd.select_by_index(inliers)
+pcd_rest = pcd.select_by_index(inliers, invert=True)
+mesh = shape.get_mesh(pcd_shape.points)
+mesh.paint_uniform_color([1, 0, 0])
+
+draw_geometries([pcd]+[mesh])
+    
+draw_geometries([pcd_rest]+[mesh])
+
