af8a2d89aa4888c9f2b457823fd1ce704035e7d9
evbernardes <evbernardes@gmail.com>
2023-11-16 10:48:51 +0100

    added utility to read hf5 files

--- pyShapeDetector/utility/__init__.py
+++ pyShapeDetector/utility/__init__.py
@@ -9,3 +9,4 @@ Created on Tue Sep 26 16:02:34 2023
 from .multidetector import MultiDetector
 from .primitivelimits import PrimitiveLimits
 from .RANSAC_Options import RANSAC_Options
+from .pointcloud_helper_functions import read_point_cloud, paint_random
---
+++ pyShapeDetector/utility/pointcloud_helper_functions.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 16:40:48 2023
+
+@author: ebernardes
+"""
+import numpy as np
+import h5py
+from pathlib import Path
+import copy
+import open3d as o3d
+from open3d.geometry import PointCloud
+from open3d.utility import Vector3dVector
+# from open3d.utility import Vect
+
+# datafolder = Path('/home/ebernardes/OneDrive_ebernardes/Documents/Data/traceparts/')
+# datafile = datafolder / '114csnb_bolt_1_2_13_2_02_1_25_10.h5'
+def read_point_cloud(filepath):
+    if isinstance(filepath, Path):
+        filename = filepath.as_posix()
+    else:
+        filename = filepath
+        filepath = Path(filename)
+
+    print(filename)
+
+    if filepath.suffix == '.h5':
+        f = h5py.File(filepath, 'r')
+        labels = np.asarray(f['gt_labels'])
+        points = f['gt_points']
+        normals = f['gt_normals']
+        pcds = []
+        for label in set(labels):
+            idx = label == labels
+            pcd = PointCloud()
+            pcd.points = Vector3dVector(points[idx])
+            pcd.normals = Vector3dVector(normals[idx])
+            pcds.append(pcd)
+        if len(pcds) == 1:
+            pcds = pcds[0]
+
+    else:
+        pcds = o3d.io.read_point_cloud(filename)
+
+    return pcds
+
+def paint_random(pcds):
+    if not isinstance(pcds, list):
+        pcds.paint_uniform_color(np.random.random(3))
+    else:
+        for pcd in pcds:
+            pcd.paint_uniform_color(np.random.random(3))
+    
+
+    
+    
+# f = h5py.File(datafile, 'r')
+
+# pcd = 
