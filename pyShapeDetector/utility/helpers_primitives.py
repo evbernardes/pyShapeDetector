2081f41bc0e0effb92c0c9022ab1465435c96d63
evbernardes <evbernardes@gmail.com>
2023-12-08 14:16:53 +0100

    documented primitive helpers

--- pyShapeDetector/utility/helpers_primitives.py
+++ pyShapeDetector/utility/helpers_primitives.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Helper functions that act on primitives.
+
 Created on Tue Dec  5 15:48:31 2023
 
 @author: ebernardes
 """
 import numpy as np
-from itertools import combinations
+from itertools import combinations, product
 # from pyShapeDetector.primitives import Plane, Cylinder
 
 def group_similar_shapes(shapes, rtol=1e-02, atol=1e-02):
+    """ Detect shapes with similar model and group.
     
-    # repeated_idx = [True]*len(shapes)
+    See: fuse_shape_groups
+    
+    Parameters
+    ----------
+    shapes : list of shapes
+        List containing all shapes.    
+    rtol : float, optional
+        The relative tolerance parameter. Default: 1e-02.
+    atol : float, optional
+        The absolute tolerance parameter. Default: 1e-02.
+        
+    Returns
+    -------
+    list of lists
+        Grouped shapes.
+    
+    """
     num_shapes = len(shapes)
     partitions = list(range(num_shapes))
     
@@ -29,6 +48,26 @@ def group_similar_shapes(shapes, rtol=1e-02, atol=1e-02):
     return sublists
 
 def fuse_shape_groups(shapes_lists, detector=None):
+    """ Find weigthed average of shapes, where the weight is the fitness
+    metric.
+    
+    If a detector is given, use it to compute the metrics of the resulting
+    average shapes.
+    
+    See: group_similar_shapes
+    
+    Parameters
+    ----------
+    shapes_lists : list of list shapes
+        Grouped shapes.
+    detector : instance of some Detector, optional
+        Used to recompute metrics.
+        
+    Returns
+    -------
+    list
+        Average shapes.    
+    """
     # num_partitions = len(shapes_lists):
     new_list = []
     for sublist in shapes_lists:
@@ -53,15 +92,29 @@ def fuse_shape_groups(shapes_lists, detector=None):
     return new_list
 
 def cut_planes_with_cylinders(shapes, radius_min, total_cut=False, eps=0):
+    """ Isolates planes and cylinders. For every plane and cylinder 
+    combination, check if cylinder cuts plane and, if it does, add a hole.
+    
+    Parameters
+    ----------
+    shapes : list of shapes
+        List containing all shapes.
+    radius_min : float
+        Only isolates cylinders with radius below this threshold.
+    total_cut : boolean, optional
+        When True, only accepts cuts when either the top of the bottom 
+        completely cuts the plane. Default: False.
+    eps : float, optional
+        Adds some backlash to top and bottom of cylinder. Default: 0.
+    
+    """
     planes = [s for s in shapes if s.name == 'plane' or s.name == 'bounded plane']
     cylinders = [s for s in shapes if s.name == 'cylinder']
     cylinders = [s for s in cylinders if s.radius < radius_min + eps]
 
-    for c in cylinders:
-        for p in planes:
-            if c.cuts(p, total_cut=total_cut, eps=eps):
-                p.add_holes(c.project_to_plane(p))
-                
+    for c, p in product(cylinders, planes):
+        if c.cuts(p, total_cut=total_cut, eps=eps):
+            p.add_holes(c.project_to_plane(p))
                 
 def get_meshes(shapes, crop_types=['sphere', 'cone']):
     """ Returns meshes from shapes.
