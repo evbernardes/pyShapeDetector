a00d2b8c03d30bf96e588c422ca57802c1eeee45
evbernardes <evbernardes@gmail.com>
2024-03-05 13:57:38 +0100

    minor

--- pyShapeDetector/utility/primitivelimits.py
+++ pyShapeDetector/utility/primitivelimits.py
@@ -8,26 +8,29 @@ Created on Fri Oct 20 17:01:30 2023
 import numpy as np
 
 class PrimitiveLimits:
+    """
+    Class to test if a shape's model is within desired limits.
     
-    def check_compatibility(self, primitive):
-        if self.args is None:
-            return 
-        for attribute in self.attribute:
-            if not hasattr(primitive, attribute):
-                raise ValueError(f"Primitive of type {primitive.name} does "
-                                 f"not have attribute {attribute}.")
-                
-    def check(self, shape):
-        if self.args is None:
-            return True
-        for arg in self.args:
-            test_value = getattr(shape, arg['attribute'])
-            if (func := arg['func']) is not None:
-                test_value = func(test_value)
-            if not (arg['limits'][0] <= test_value <= arg['limits'][1]):
-                return False
-            
-        return True
+    First, define limits instance and primitive:
+    
+    >>> from pyShapeDetector.primitives import Sphere, Plane
+    >>> from pyShapeDetector.utility import PrimitiveLimits
+    >>> limits = PrimitiveLimits(['radius', [0, 2]])
+    >>> sphere1 = Sphere.from_center_radius([0, 0, 0], 1.5)
+    >>> sphere2 = Sphere.from_center_radius([0, 0, 0], 3)
+    
+    This instance will test if a shape has a 'radius' is in the closed interval
+    [0, 2]. In order to apply the check:
+    
+    >>>
+    
+    Attributes
+    ----------
+        
+    Methods
+    -------
+    __init__
+    """
         
     @property
     def func(self):
@@ -47,21 +50,12 @@ class PrimitiveLimits:
             return None
         return [a['limits'] for a in self.args]
     
-    
     @property
     def args_list(self):
         # func, attribute, limit_type, value = zip(self.args_list)
         return self.func, self.attribute, self.value
     
-    def __add__(self, limits_other):
-        assert isinstance(limits_other, PrimitiveLimits)
-        
-        limits_sum = PrimitiveLimits(None)
-        limits_sum.args = self.args + limits_other.args
-        return limits_sum            
-    
     def __init__(self, args):
-        
         self.args = []
         
         if args is None:
@@ -93,7 +87,7 @@ class PrimitiveLimits:
                     raise ValueError("limits must be a list or tuple of 2 "
                                      f"elements, got {limits}")
                     
-                if limits[0] != None and limits[1] != None:
+                if limits[0] is None and limits[1] is None:
                     raise ValueError(f"Limit ranges cannot all be None, got {limits}.")
                     
                 if limits[0] is None:
@@ -113,3 +107,30 @@ class PrimitiveLimits:
                     }
                     
                 self.args.append(limits_dict)
+                
+    def check_compatibility(self, primitive):
+        if self.args is None:
+            return 
+        for attribute in self.attribute:
+            if not hasattr(primitive, attribute):
+                raise ValueError(f"Primitive of type {primitive.name} does "
+                                 f"not have attribute {attribute}.")
+                
+    def check(self, shape):
+        if self.args is None:
+            return True
+        for arg in self.args:
+            test_value = getattr(shape, arg['attribute'])
+            if (func := arg['func']) is not None:
+                test_value = func(test_value)
+            if not (arg['limits'][0] <= test_value <= arg['limits'][1]):
+                return False
+            
+        return True
+    
+    def __add__(self, limits_other):
+        assert isinstance(limits_other, PrimitiveLimits)
+        
+        limits_sum = PrimitiveLimits(None)
+        limits_sum.args = self.args + limits_other.args
+        return limits_sum
