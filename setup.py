da8bd5b911b14c963eacc1a56d52920e0206912e
evbernardes <evbernardes@gmail.com>
2024-03-14 08:55:26 +0100

    not sure

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-from distutils.core import setup
+# from distutils.core import setup
+# setup(name='pyShapeDetector', version='0.0.1dev', packages=['pyShapeDetector'])
+from setuptools import setup, find_packages
 
-setup(name='pyShapeDetector', version='0.0.1dev', packages=['pyShapeDetector'])
+setup(
+    name='pyShapeDetector',
+    version='0.0.1dev',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.25.2',
+        'open3d==0.17.0',
+    ],
+    author='Evandro Bernardes',
+    author_email='evbernardes@gmail.com',
+    description='Python 3 module to detect geometrical primitives in pointclouds',
+    license='MIT',
+    url='https://github.com/evbernardes/pyShapeDetector',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
