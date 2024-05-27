2e6ca65e1363ce3523982cc3f14dc80d6324ae51
evbernardes <evbernardes@gmail.com>
2024-05-27 10:53:58 +0200

    minor fix

--- pyShapeDetector/primitives/tests/test_primitives.py
+++ pyShapeDetector/primitives/tests/test_primitives.py
@@ -540,7 +540,7 @@ def test_axis_aligned_bounding_box_planes():
     #         pcd = plane.inlier_PointCloud.crop(plane.bbox)
     #         assert len(pcd.points) == num_samples
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # test_plane_transformations()
     # test_distances()
     # test_equal()
