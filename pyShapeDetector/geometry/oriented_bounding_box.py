83471c78b2f72e2150b83be5ce6395ad92e3847c
evbernardes <evbernardes@gmail.com>
2024-06-24 16:07:46 +0200

    Implemented contains_points for OrientedBoundingBox

--- pyShapeDetector/geometry/oriented_bounding_box.py
+++ pyShapeDetector/geometry/oriented_bounding_box.py
@@ -5,9 +5,12 @@ Created on Wed Jun  5 15:27:03 2024
 
 @author: ebernardes
 """
+import numpy as np
 from open3d.geometry import OrientedBoundingBox as open3d_OrientedBoundingBox
 
+from pyShapeDetector.utility import _set_and_check_3d_array
 from .open3d_geometry import link_to_open3d_geometry, Open3D_Geometry
+from .axis_aligned_bounding_box import AxisAlignedBoundingBox
 
 
 @link_to_open3d_geometry(open3d_OrientedBoundingBox)
@@ -19,9 +22,32 @@ class OrientedBoundingBox(Open3D_Geometry):
 
     Methods
     -------
+    contains_points
     expanded
 
     """
+
+    def contains_points(self, points, inclusive=True, eps=1e-5):
+        """
+        Check which points are inside of the bounding box.
+
+        Parameters
+        ----------
+
+        points : N x 3 array
+            N input points
+
+        Returns
+        -------
+        Numpy array
+            Boolean values
+        """
+        points = _set_and_check_3d_array(points, name="points")
+        points = (points - self.center) @ self.R
+        extent = self.extent / 2 + eps
+        aabb = AxisAlignedBoundingBox(-extent, +extent)
+
+        return aabb.contains_points(points, inclusive=inclusive)
 
     def expanded(self, slack=0):
         """Return expanded version with bounds expanded in all directions.
