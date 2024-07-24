Corrected PlaneBounded and PlaneTriangulated fusion, not fuse sets color to average

pyShapeDetector/primitives/planetriangulated.py
@@ -420,7 +420,7 @@ class PlaneTriangulated(Plane):
         PlaneTriangulated
             Averaged PlaneTriangulated instance.
         """
-        plane_unbounded = Plane.fuse(shapes, detector, ignore_extra_data)
+        plane_unbounded = Plane.fuse(shapes, detector, ignore_extra_data=True)
 
         vertices_list = [plane_unbounded.flatten_points(s.vertices) for s in shapes]
         triangles_list = [s.triangles for s in shapes]
@@ -431,8 +431,17 @@ class PlaneTriangulated(Plane):
 
         shape = PlaneTriangulated(plane_unbounded.model, vertices, triangles)
         if not ignore_extra_data:
-            shape.set_inliers(plane_unbounded)
-            shape.metrics = plane_unbounded.metrics
+            pcd = PointCloud.fuse_pointclouds([shape.inliers for shape in shapes])
+            shape.set_inliers(pcd)
+            shape.color = np.mean([s.color for s in shapes], axis=0)
+
+            if detector is not None:
+                num_points = sum([shape.metrics["num_points"] for shape in shapes])
+                num_inliers = len(pcd.points)
+                distances, angles = shape.get_residuals(pcd.points, pcd.normals)
+                shape.metrics = detector.get_metrics(
+                    num_points, num_inliers, distances, angles
+                )
 
         return shape
 