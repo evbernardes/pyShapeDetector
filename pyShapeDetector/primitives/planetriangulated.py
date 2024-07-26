689a882d94b6f161345af6f65ddb0137ce99ae62
evbernardes <evbernardes@gmail.com>
2024-07-26 16:18:24 +0200

    Setting mesh when defining PlaneTriangulated

--- pyShapeDetector/primitives/planetriangulated.py
+++ pyShapeDetector/primitives/planetriangulated.py
@@ -618,7 +618,7 @@ class PlaneTriangulated(Plane):
             If False, does not flatten points
 
         """
-        self._mesh = None
+        # self._mesh = None
         vertices = np.asarray(vertices)
         triangles = np.asarray(triangles)
 
@@ -643,6 +643,7 @@ class PlaneTriangulated(Plane):
         # self._bounds_indices = np.array([])
         self._vertices = vertices
         self._triangles = triangles
+        self._mesh = self.get_mesh()
         # self._convex = False
 
     @staticmethod
