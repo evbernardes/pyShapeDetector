ee0e42376067d9ea0d9dd0d1cd31467399685756
evbernardes <evbernardes@gmail.com>
2024-08-09 15:03:42 +0200

    Added file version to file save

--- pyShapeDetector/primitives/primitivebase.py
+++ pyShapeDetector/primitives/primitivebase.py
@@ -1211,6 +1211,7 @@ class Primitive(ABC):
                 break
 
     def __put_attributes_in_dict__(self, data, save_inliers=True):
+        data["file_version"] = 0.1
         data["name"] = self.name
         data["model"] = self.model.tolist()
         if save_inliers:
