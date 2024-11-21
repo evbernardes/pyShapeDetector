ac7ddff70a6c1ca15262924d6271aa2956b18b04
evbernardes <evbernardes@gmail.com>
2024-11-21 13:01:09 +0100

    Fixed some tests

--- pyShapeDetector/utility/detector_options.py
+++ pyShapeDetector/utility/detector_options.py
@@ -17,53 +17,53 @@ class DetectorOptions:
 
     _reduction_rate = 1.0
     _threshold_distance = None
+    # _max_point_distance = None
     _threshold_angle = 3.141592653589793  # ~ 180 degrees
+    # _max_normal_angle_degrees = 10
     _threshold_ratios = [0.2, 0.7]  # for LDSAC
     _threshold_refit_ratio = 1
-    _num_samples = None
-    _num_iterations = 100
     _probability = 0.99999
-    _max_point_distance = None
-    _max_normal_angle_degrees = 10
-    _inliers_min = None
     _fitness_min = None
     _connected_components_eps = None
-    _downsample = 1
     _adaptative_threshold_k = 15
+    _num_samples = None
+    _num_iterations = 100
+    _inliers_min = None
+    _downsample = 1
 
     _valid_parameters = [
         "_reduction_rate",
         "reduction_rate",
         "_threshold_distance",
         "threshold_distance",
+        # "_max_point_distance",
+        # "max_point_distance",
         "_threshold_angle",
         "threshold_angle",
         # no "_threshold_angle_degrees", it's an internal call to "_threshold_angle"
         "threshold_angle_degrees",
+        # "_max_normal_angle_degrees",
+        # "max_normal_angle_degrees",
         "_threshold_ratios",
         "threshold_ratios",
         "_threshold_refit_ratio",
         "threshold_refit_ratio",
-        "_num_samples",
-        "num_samples",
-        "_num_iterations",
-        "num_iterations",
         "_probability",
         "probability",
-        "_max_point_distance",
-        "max_point_distance",
-        "_max_normal_angle_degrees",
-        "max_normal_angle_degrees",
-        "_inliers_min",
-        "inliers_min",
         "_fitness_min",
         "fitness_min",
         "_connected_components_eps",
         "connected_components_eps",
-        "_downsample",
-        "downsample",
         "_adaptative_threshold_k",
         "adaptative_threshold_k",
+        "_num_samples",
+        "num_samples",
+        "_num_iterations",
+        "num_iterations",
+        "_inliers_min",
+        "inliers_min",
+        "_downsample",
+        "downsample",
     ]
 
     # @property
@@ -76,7 +76,11 @@ class DetectorOptions:
 
     @property
     def dict(self):
-        return {name: self.__getattribute__(name) for name in self._valid_parameters}
+        return {
+            name: self.__getattribute__(name)
+            for name in self._valid_parameters
+            if name[0] != "_"
+        }
 
     def __repr__(self):
         lines = "\n".join("{!r}: {!r},".format(k, v) for k, v in self.dict.items())
@@ -105,8 +109,16 @@ class DetectorOptions:
     @threshold_distance.setter
     def threshold_distance(self, value):
         if value is not None and value < 0:
-            raise ValueError("threshold_distance must be non-negative.")
+            raise ValueError("threshold_distance must be None or non-negative.")
         self._threshold_distance = value
+
+    # @property
+    # def max_point_distance(self):
+    #     return self._max_point_distance
+
+    # @max_point_distance.setter
+    # def max_point_distance(self, value):
+    #     self._max_point_distance = value
 
     @property
     def threshold_angle(self):
@@ -118,15 +130,21 @@ class DetectorOptions:
             raise ValueError("threshold_angle must be non-negative.")
         self._threshold_angle = value
 
+    # @property
+    # def max_normal_angle_degrees(self):
+    #     return self._max_normal_angle_degrees
+
+    # @max_normal_angle_degrees.setter
+    # def max_normal_angle_degrees(self, value):
+    #     self._max_normal_angle_degrees = value
+
     @property
     def threshold_angle_degrees(self):
         return np.rad2deg(self.threshold_angle)
 
     @threshold_angle_degrees.setter
     def threshold_angle_degrees(self, value):
-        if value < 0:
-            raise ValueError("threshold_angle_degrees must be positive")
-        self._threshold_angle = np.deg2rad(value)
+        self.threshold_angle = np.deg2rad(value)
 
     @property
     def threshold_ratios(self):
@@ -163,13 +181,59 @@ class DetectorOptions:
         self._threshold_refit_ratio = value
 
     @property
+    def probability(self):
+        return self._probability
+
+    @probability.setter
+    def probability(self, value):
+        if value < 0 or value >= 1:
+            raise ValueError(f"probability must be in range [0, 1), got {value}")
+        self._probability = value
+
+    @property
+    def fitness_min(self):
+        return self._fitness_min
+
+    @fitness_min.setter
+    def fitness_min(self, value):
+        if value is not None and (value < 0 or value > 1):
+            raise ValueError(
+                f"fitness_min must be None or in range [0, 1], got {value}"
+            )
+        self._fitness_min = value
+
+    @property
+    def connected_components_eps(self):
+        return self._connected_components_eps
+
+    @connected_components_eps.setter
+    def connected_components_eps(self, value):
+        if value is not None and value < 0:
+            raise ValueError("connected_components_eps must be None or non-negative.")
+        self._connected_components_eps = value
+
+    @property
+    def adaptative_threshold_k(self):
+        return self._adaptative_threshold_k
+
+    @adaptative_threshold_k.setter
+    def adaptative_threshold_k(self, value):
+        if not isinstance(value, int) or value < 1:
+            raise ValueError(
+                "adaptative_threshold_k must be a positive integer, got {value}."
+            )
+        self._adaptative_threshold_k = value
+
+    @property
     def num_samples(self):
         return self._num_samples
 
     @num_samples.setter
     def num_samples(self, value):
-        if not isinstance(value, int) or value < 1:
-            raise ValueError(f"num_samples must be a positive integer, got {value}.")
+        if value is not None and (not isinstance(value, int) or value < 1):
+            raise ValueError(
+                f"num_samples must be None or a positive integer, got {value}."
+            )
         self._num_samples = value
 
     @property
@@ -183,60 +247,16 @@ class DetectorOptions:
         self._num_iterations = value
 
     @property
-    def probability(self):
-        return self._probability
-
-    @probability.setter
-    def probability(self, value):
-        if value < 0 or value >= 1:
-            raise ValueError(f"probability must be in range [0, 1), got {value}")
-        self._probability = value
-
-    @property
-    def max_point_distance(self):
-        return self._max_point_distance
-
-    @max_point_distance.setter
-    def max_point_distance(self, value):
-        self._max_point_distance = value
-
-    @property
-    def max_normal_angle_degrees(self):
-        return self._max_normal_angle_degrees
-
-    @max_normal_angle_degrees.setter
-    def max_normal_angle_degrees(self, value):
-        self._max_normal_angle_degrees = value
-
-    @property
     def inliers_min(self):
         return self._inliers_min
 
     @inliers_min.setter
     def inliers_min(self, value):
-        if not isinstance(value, int) or value < 1:
-            raise ValueError(f"inliers_min must be a positive integer, got {value}.")
+        if value is not None and (not isinstance(value, int) or value < 1):
+            raise ValueError(
+                f"inliers_min must be None or a positive integer, got {value}."
+            )
         self._inliers_min = value
-
-    @property
-    def fitness_min(self):
-        return self._fitness_min
-
-    @fitness_min.setter
-    def fitness_min(self, value):
-        if value and (value < 0 or value > 1):
-            raise ValueError(f"fitness_min must be in range [0, 1], got {value}")
-        self._fitness_min = value
-
-    @property
-    def connected_components_eps(self):
-        return self._connected_components_eps
-
-    @connected_components_eps.setter
-    def connected_components_eps(self, value):
-        if value is not None and value < 0:
-            raise ValueError("connected_components_eps must be non-negative.")
-        self._connected_components_eps = value
 
     @property
     def downsample(self):
@@ -247,18 +267,6 @@ class DetectorOptions:
         if not isinstance(value, int) or value < 1:
             raise ValueError("downsample must be a positive integer, got {value}.")
         self._downsample = value
-
-    @property
-    def adaptative_threshold_k(self):
-        return self._adaptative_threshold_k
-
-    @adaptative_threshold_k.setter
-    def adaptative_threshold_k(self, value):
-        if not isinstance(value, int) or value < 1:
-            raise ValueError(
-                "adaptative_threshold_k must be a positive integer, got {value}."
-            )
-        self._adaptative_threshold_k = value
 
     # Only allows setting valid arguments
     def __setattr__(self, name, value):
--- pyShapeDetector/utility/tests/test_detector_options.py
+++ pyShapeDetector/utility/tests/test_detector_options.py
@@ -19,7 +19,7 @@ def test_invalid_parameters():
     # This works
     options.threshold_angle_degrees = 10
 
-    with pytest.raises(AttributeError):
+    with pytest.raises(AttributeError, match="Cannot set undefined parameter"):
         # This doesn't, typo in parameter
         options.threshold_angle_degree = 10
 
@@ -27,16 +27,20 @@ def test_invalid_parameters():
 def test_threshold_distance():
     options.threshold_distance = 1
     options.threshold_distance = 0
-    with pytest.raises(ValueError, match="must be non-negative"):
+    options.threshold_distance = None
+
+    with pytest.raises(ValueError, match="must be None or non-negative"):
         options.threshold_distance = -1
 
 
 def test_threshold_angle():
     options.threshold_angle = 1
     options.threshold_angle = 0
-    with pytest.raises(ValueError):
+
+    with pytest.raises(ValueError, match="must be non-negative"):
         options.threshold_angle = -1
-    with pytest.raises(ValueError):
+
+    with pytest.raises(ValueError, match="must be non-negative"):
         options.threshold_angle_degrees = -10
 
     options.threshold_angle_degrees = 10
@@ -73,17 +77,70 @@ def test_refit_ratio():
         options.threshold_refit_ratio = 0.9
 
 
+def test_probability():
+    options.probability = 0.1
+    options.probability = 0.9
+    options.probability = 0
+
+    with pytest.raises(ValueError, match="must be in range"):
+        options.probability = -0.1
+
+    with pytest.raises(ValueError, match="must be in range"):
+        options.probability = 1.1
+
+    with pytest.raises(ValueError, match="must be in range"):
+        options.probability = 1
+
+
+def test_fitness_min():
+    options.fitness_min = 0.1
+    options.fitness_min = 0.9
+    options.fitness_min = 0
+    options.fitness_min = 1
+    options.fitness_min = None
+
+    with pytest.raises(ValueError, match="must be None or in range"):
+        options.fitness_min = -0.1
+
+    with pytest.raises(ValueError, match="must be None or in range"):
+        options.fitness_min = 1.1
+
+
+def test_connected_components_eps():
+    options.connected_components_eps = 1
+    options.connected_components_eps = 0
+    options.connected_components_eps = None
+
+    with pytest.raises(ValueError, match="must be None or non-negative"):
+        options.connected_components_eps = -1
+
+
+def test_adaptative_threshold_k():
+    options.adaptative_threshold_k = 10
+    options.adaptative_threshold_k = 100
+
+    with pytest.raises(ValueError, match="must be a positive integer"):
+        options.adaptative_threshold_k = 3.9
+
+    with pytest.raises(ValueError, match="must be a positive integer"):
+        options.adaptative_threshold_k = 0
+
+    with pytest.raises(ValueError, match="must be a positive integer"):
+        options.adaptative_threshold_k = -1
+
+
 def test_num_samples():
     options.num_samples = 10
     options.num_samples = 100
+    options.num_samples = None
 
-    with pytest.raises(ValueError, match="must be a positive integer"):
+    with pytest.raises(ValueError, match="must be None or a positive integer"):
         options.num_samples = 3.9
 
-    with pytest.raises(ValueError, match="must be a positive integer"):
+    with pytest.raises(ValueError, match="must be None or a positive integer"):
         options.num_samples = 0
 
-    with pytest.raises(ValueError, match="must be a positive integer"):
+    with pytest.raises(ValueError, match="must be None or a positive integer"):
         options.num_samples = -1
 
 
@@ -101,53 +158,19 @@ def test_num_iterations():
         options.num_iterations = -1
 
 
-def test_probability():
-    options.probability = 0.1
-    options.probability = 0.9
-    options.probability = 0
-
-    with pytest.raises(ValueError, match="must be in range"):
-        options.probability = -0.1
-
-    with pytest.raises(ValueError, match="must be in range"):
-        options.probability = 1.1
-
-    with pytest.raises(ValueError, match="must be in range"):
-        options.probability = 1
-
-
 def test_inliers_min():
     options.inliers_min = 10
     options.inliers_min = 100
+    options.num_samples = None
 
-    with pytest.raises(ValueError, match="must be a positive integer"):
+    with pytest.raises(ValueError, match="must be None or a positive integer"):
         options.inliers_min = 3.9
 
-    with pytest.raises(ValueError, match="must be a positive integer"):
+    with pytest.raises(ValueError, match="must be None or a positive integer"):
         options.inliers_min = 0
 
-    with pytest.raises(ValueError, match="must be a positive integer"):
+    with pytest.raises(ValueError, match="must be None or a positive integer"):
         options.inliers_min = -1
-
-
-def test_fitness_min():
-    options.fitness_min = 0.1
-    options.fitness_min = 0.9
-    options.fitness_min = 0
-    options.fitness_min = 1
-
-    with pytest.raises(ValueError, match="must be in range"):
-        options.fitness_min = -0.1
-
-    with pytest.raises(ValueError, match="must be in range"):
-        options.fitness_min = 1.1
-
-
-def test_connected_components_eps():
-    options.connected_components_eps = 1
-    options.connected_components_eps = 0
-    with pytest.raises(ValueError, match="must be non-negative"):
-        options.connected_components_eps = -1
 
 
 def test_downsample():
@@ -162,17 +185,3 @@ def test_downsample():
 
     with pytest.raises(ValueError, match="must be a positive integer"):
         options.downsample = -1
-
-
-def test_adaptative_threshold_k():
-    options.adaptative_threshold_k = 10
-    options.adaptative_threshold_k = 100
-
-    with pytest.raises(ValueError, match="must be a positive integer"):
-        options.adaptative_threshold_k = 3.9
-
-    with pytest.raises(ValueError, match="must be a positive integer"):
-        options.adaptative_threshold_k = 0
-
-    with pytest.raises(ValueError, match="must be a positive integer"):
-        options.adaptative_threshold_k = -1
