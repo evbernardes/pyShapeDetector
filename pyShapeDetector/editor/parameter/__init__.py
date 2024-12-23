7345d49f4636e64e8df60c90ac83da6ef59c9b12
evbernardes <evbernardes@gmail.com>
2024-12-23 11:15:23 +0100

    minor

--- pyShapeDetector/editor/parameter/__init__.py
+++ pyShapeDetector/editor/parameter/__init__.py
@@ -10,16 +10,12 @@ from numpy import ndarray
 from open3d.visualization.gui import Color
 
 from .parameter import ParameterBase
-from .parameter_panel import ParameterPanel
-
 from .parameter_bool import ParameterBool
 from .parameter_options import ParameterOptions
-
-# from .parameter_float import ParameterFloat
-# from .parameter_int import ParameterInt
 from .parameter_numeric import ParameterNumeric
 from .parameter_color import ParameterColor
 from .parameter_ndarray import ParameterNDArray
+from .parameter_panel import ParameterPanel
 
 PARAMETER_TYPE_DICTIONARY = {
     None: ParameterBase,
