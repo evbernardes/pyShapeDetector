c34535d79a1f469bbbaca34d2834e96c85acbf85
evbernardes <evbernardes@gmail.com>
2025-01-08 16:01:06 +0100

    getting version

--- pyShapeDetector/__init__.py
+++ pyShapeDetector/__init__.py
@@ -6,3 +6,9 @@ Created on Tue Sep 26 16:02:34 2023
 @author: ebernardes
 """
 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pyShapeDetector")
+except PackageNotFoundError:
+    __version__ = "unknown"
